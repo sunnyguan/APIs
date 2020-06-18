d2a384c39dd683b0420a8a9458a2e535607b5555
sunnyguan <sunny.peng.guan@gmail.com>
2020-06-18 13:54:49 -0700

    hm

--- app/main.py
+++ app/main.py
@@ -15,15 +15,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
+"""
+app = Flask(__name__)
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+"""
+
 app = Flask(__name__)
 CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
 options = webdriver.ChromeOptions()
 options.binary_location = chrome_bin
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
-options.add_argument('headless')
-options.add_argument('window-size=1200x600')
+options.add_argument('--headless')
+# options.add_argument('window-size=1200x600')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
 def close_running_threads():
@@ -94,27 +105,34 @@ def course_api():
     driver.find_element_by_id("srch").send_keys(Keys.RETURN)
     # time.sleep(3)
     table = 0
-    while True:
+    ki = 0
+    while ki <= 30:
         try:
             table = driver.find_element_by_xpath("//table/tbody")
             break
         except Exception as e:
             print('wait...')
+            ki += 1
             time.sleep(0.3)
-    rows = table.find_elements_by_tag_name("tr")
-    text = []
-    j = 1
-    for row in rows:
-        curr = {}
-        col = row.find_elements_by_tag_name("td")
-        for i in range(0, len(col)):
-            curr[i] = col[i].text
-        text.append(curr)
-        j+=1
-    print(text)
-    resp = jsonify(text)
-    resp.status_code = 200
-    return resp
+    if(ki == 30):
+        resp = jsonify({"bad": "true"})
+        resp.status_code = 200
+        return resp
+    else:
+        rows = table.find_elements_by_tag_name("tr")
+        text = []
+        j = 1
+        for row in rows:
+            curr = {}
+            col = row.find_elements_by_tag_name("td")
+            for i in range(0, len(col)):
+                curr[i] = col[i].text
+            text.append(curr)
+            j+=1
+        print(text)
+        resp = jsonify(text)
+        resp.status_code = 200
+        return resp
 
 @app.route("/api/face", methods=['POST'])
 def detect_face():
